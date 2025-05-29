d496bc8e0461c3938d55fe6160f2f6402e8fcbfa
Artem Udovyk <artem@udovyk.com>
2025-05-29 17:42:06 +0200

    remove houdini

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import houdini from "houdini/vite";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [houdini(), sveltekit()]
+	plugins: [sveltekit()]
 });
